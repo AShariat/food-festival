f5d8f4252e650f75c03612c523b0b4a3d5f5281a
Amir Shariat <amirshariat1987@gmail.com>
2022-05-03 15:23:29 -0600

    added offline feature

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ const BundleAnalyzerPlugin =
   require("webpack-bundle-analyzer").BundleAnalyzerPlugin;
 
 // For a basic configuration, we need to provide webpack with three properties: entry, output, and mode. The first thing we want to declare is the entry property.
-module.exports = {
+const config = {
   // The entry point is the root of the bundle and the beginning of the dependency graph, so give it the relative path to the client's code.
   entry: {
     app: "./assets/js/script.js",
@@ -15,13 +15,13 @@ module.exports = {
   // webpack will next take the entry point we have provided, bundle that code, and output that bundled code to a folder that we specify. It is common and best practice to put your bundled code into a folder named dist, which is short for distribution.
   output: {
     filename: "[name].bundle.js",
-    path: __dirname + "/dist",
+    path: `${__dirname}/dist`,
   },
   // We added an object to the rules array. This object will identify the type of files to pre-process using the test property to find a regular expression, or regex. In our case, we are trying to process any image file with the file extension of .jpg. We could expand this expression to also search for other image file extensions such as .png, .svg, or .gif.
   module: {
     rules: [
       {
-        test: /\.jpg$/i,
+        test: /\.(png|jpe?g|gif)$/i,
         use: [
           {
             loader: "file-loader",
@@ -59,3 +59,5 @@ module.exports = {
   // The final piece of our basic setup will provide the mode in which we want webpack to run. By default, webpack wants to run in production mode. In this mode, webpack will minify our code for us automatically, along with some other nice additions. We want our code to run in development mode.
   mode: "development",
 };
+
+module.exports = config;
